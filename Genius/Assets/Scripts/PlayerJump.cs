50bc5e69d2ba06bcca8dcfa9032d888724c494e9
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-08-22 00:35:05 -0300

    Andar e pular

---
+++ Genius/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private string GroundCheckName;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pular();
+    }
+    bool IsGrounded()
+    {
+        return transform.Find(GroundCheckName).GetComponent<GroundCheck>().isGrounded;
+    }
+    void Pular()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity += Vector2.up * jumpForce;
+        }
+
+    }
+}
---
+++ Genius/Assets/Scripts/PlayerWalk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    [SerializeField] private float vel = 1f;
+    [SerializeField] private string ipt;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        float aux = Input.GetAxis(ipt);
+        if (aux > 0)
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+            rb.velocity = new Vector2(vel, rb.velocity.y) ;
+        }
+        else if (aux < 0)
+        {
+            transform.rotation = new Quaternion(0, 180, 0, 0);
+            rb.velocity = new Vector2(-vel, rb.velocity.y);
+        }
+        else
+            rb.velocity = new Vector2(0, rb.velocity.y);
+    }
+}
