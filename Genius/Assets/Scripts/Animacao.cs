bb7f55b4e1a0624b12a7acad0ce69f3cf2972b8b
Jean Tan Li <30798045+JeanTL190@users.noreply.github.com>
2020-08-23 17:29:02 -0300

    Prefabs

---
+++ Genius/Assets/Scripts/Animacao.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animacao : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private GroundCheck gc;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        gc = GetComponentInChildren<GroundCheck>();
+    }
+    private void Update()
+    {
+        anim.SetFloat("Speed", rb.velocity.normalized.sqrMagnitude);
+        anim.SetBool("Jump", !gc.isGrounded);
+    }
+}
